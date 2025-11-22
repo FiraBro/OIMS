de873e4ba59cc93ad6c48278fc3880db4bc77f1c
Firagos Jemal <jemalfiragos@gmail.com>
2025-11-22 22:19:17 +0300

    simple modification

---
+++ Server/middlewares/authMiddleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
+export const protect = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-password");
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Token failed or expired", error });
+  }
+};
+
+export const isAdmin = (req, res, next) => {
+  if (req.user?.role !== "admin") {
+    return res.status(403).json({ message: "Access denied. Admin only." });
+  }
+  next();
+};

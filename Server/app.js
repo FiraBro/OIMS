a299c068bad04e510ed71720720d4ff1e45f72dc
Firagos Jemal <jemalfiragos@gmail.com>
2025-11-22 22:43:50 +0300

    add global error handler to it

--- Server/app.js
+++ Server/app.js
@@ -10,26 +10,37 @@ import PolicyRouter from "./routes/policyAplicationRoutes.js";
 import claimRouter from "./routes/claimRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 
-// Load env
+import AppError from "./utils/AppError.js";
+import globalErrorHandler from "./middlewares/errorHandler.js";
+
+// Load environment variables
 dotenv.config();
 
 const app = express();
 
-// Middleware
+// ===== Middleware =====
 app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
-// Home route
+// ===== Home Route =====
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// Routes
+// ===== API Routes =====
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/plans", insurancePlanRouter);
 app.use("/api/v1/policy", PolicyRouter);
 app.use("/api/v1/claims", claimRouter);
 app.use("/api/v1/user", userRouter);
+
+// ===== 404 Handler (No *) =====
+app.use((req, res, next) => {
+  next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404));
+});
+
+// ===== Global Error Handler =====
+app.use(globalErrorHandler);
 
 export default app;

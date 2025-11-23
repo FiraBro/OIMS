512d7577f0d645f7cc31ec3e34d925e3eeb74417
Firagos Jemal <jemalfiragos@gmail.com>
2025-11-23 11:44:28 +0300

    adding the logout

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -2,11 +2,12 @@ import catchAsync from "../utils/catchAsync.js";
 import {
   registerUser,
   loginUser,
+  logoutUser,
   forgotPasswordService,
   resetPasswordService,
 } from "../services/authService.js";
 
-export const register = catchAsync(async (req, res, next) => {
+export const register = catchAsync(async (req, res) => {
   const user = await registerUser(req.body);
 
   res.status(201).json({
@@ -16,7 +17,7 @@ export const register = catchAsync(async (req, res, next) => {
   });
 });
 
-export const login = catchAsync(async (req, res, next) => {
+export const login = catchAsync(async (req, res) => {
   const result = await loginUser(req.body);
 
   res.status(200).json({
@@ -25,8 +26,24 @@ export const login = catchAsync(async (req, res, next) => {
     ...result,
   });
 });
+export const logout = catchAsync(async (req, res) => {
+  const { refreshToken } = req.body; // make sure the client sends the refresh token
+  if (!refreshToken) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Refresh token is required",
+    });
+  }
 
-export const forgotPassword = catchAsync(async (req, res, next) => {
+  const result = await logoutUser(refreshToken);
+
+  res.status(200).json({
+    status: "success",
+    message: result.message,
+  });
+});
+
+export const forgotPassword = catchAsync(async (req, res) => {
   const resetURL = await forgotPasswordService(req.body.email);
 
   res.status(200).json({
@@ -36,7 +53,7 @@ export const forgotPassword = catchAsync(async (req, res, next) => {
   });
 });
 
-export const resetPassword = catchAsync(async (req, res, next) => {
+export const resetPassword = catchAsync(async (req, res) => {
   const result = await resetPasswordService(
     req.params.token,
     req.body.password

0c59c67b452490a8056a1dede1fe1c1831d50984
Firagos Jemal <jemalfiragos@gmail.com>
2025-11-23 20:59:17 +0300

    add rate limit

---
+++ Server/middlewares/rateLimit.js
@@ -0,0 +1,10 @@
+// middleware/rateLimit.js
+import rateLimit from "express-rate-limit";
+
+export const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: { status: "fail", message: "Too many requests. Try later." },
+  legacyHeaders: false,
+  standardHeaders: true,
+});

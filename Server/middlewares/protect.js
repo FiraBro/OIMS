681f409220591b75432a21469c9507f763070d46
Firagos Jemal <jemalfiragos@gmail.com>
2025-11-23 20:58:51 +0300

    rename it

---
+++ Server/middlewares/protect.js
@@ -0,0 +1,30 @@
+// middlewares/protect.js
+import { verifyAccessToken } from "../utils/jwt.js";
+import AppError from "../utils/AppError.js";
+import User from "../models/user.js";
+
+// Protect route (check JWT)
+export const protect = async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken || req.headers.authorization?.split(" ")[1];
+    if (!token) throw new AppError("Not logged in", 401);
+
+    const payload = verifyAccessToken(token);
+    const currentUser = await User.findById(payload.id);
+    if (!currentUser) throw new AppError("User no longer exists", 401);
+
+    req.user = { id: currentUser._id, role: currentUser.role };
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Check admin role
+export const isAdmin = (req, res, next) => {
+  if (!req.user || req.user.role !== "admin") {
+    return next(new AppError("Admin access required", 403));
+  }
+  next();
+};

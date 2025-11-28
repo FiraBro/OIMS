a1b1a09806538b92b9e04689dbfb47e0b55b778e
Firagos Jemal <jemalfiragos@gmail.com>
2025-11-28 21:12:17 +0300

    creating status code

---
+++ Server/constants/statusCodes.js
@@ -0,0 +1,10 @@
+// Status codes for plans, policies, claims
+export const STATUS_CODES = {
+  PUBLISHED: "published",
+  DRAFT: "draft",
+  ACTIVE: "active",
+  EXPIRED: "expired",
+  APPROVED: "approved",
+  REJECTED: "rejected",
+  PENDING: "pending",
+};

9e7510e5f691ca6e4b24de6c4a55a5581973d241
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-07 11:23:51 +0300

    removing margin top

--- Clients/src/components/footer/Footer.jsx
+++ Clients/src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-100 py-6 text-center text-gray-600 text-sm mt-10">
+    <footer className="bg-gray-100 py-6 text-center text-gray-600 text-sm">
       &copy; {new Date().getFullYear()} InsureWise. All rights reserved.
     </footer>
   );

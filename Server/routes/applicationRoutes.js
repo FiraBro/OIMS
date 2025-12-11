6f64d8e5e070f0e59fbb0a024d5b3fb96cf15ec3
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-11 23:01:04 +0300

    modification

--- Server/routes/applicationRoutes.js
+++ Server/routes/applicationRoutes.js
@@ -1,35 +1,49 @@
 import express from "express";
 import {
   applyForPolicy,
-  getMyApplications,
   approveApplication,
   rejectApplication,
+  getMyApplications,
   listApplications,
   deleteApplication,
 } from "../controllers/applicationController.js";
+
 import { protect, restrictTo } from "../middlewares/protect.js";
 import { ROLES } from "../constants/roles.js";
 
 const router = express.Router();
 
+// ===========================
 // USER ROUTES
-router.post("/", protect, applyForPolicy);
-router.get("/mine", protect, getMyApplications);
+// ===========================
+router.post("/apply", protect, restrictTo(ROLES.USER), applyForPolicy);
 
+router.get(
+  "/my-applications",
+  protect,
+  restrictTo(ROLES.USER),
+  getMyApplications
+);
+
+// ===========================
 // ADMIN ROUTES
+// ===========================
 router.get("/", protect, restrictTo(ROLES.ADMIN), listApplications);
+
 router.patch(
-  "/:id/approve",
+  "/approve/:id",
   protect,
   restrictTo(ROLES.ADMIN),
   approveApplication
 );
+
 router.patch(
-  "/:id/reject",
+  "/reject/:id",
   protect,
   restrictTo(ROLES.ADMIN),
   rejectApplication
 );
+
 router.delete("/:id", protect, restrictTo(ROLES.ADMIN), deleteApplication);
 
 export default router;

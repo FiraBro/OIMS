4977e1262d39ec1e46ef774c9fd6e90be57ea863
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-29 00:04:08 +0300

    add some field

--- Server/models/claim.js
+++ Server/models/claim.js
@@ -4,7 +4,8 @@ const claimSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   policyNumber: { type: String, required: true },
   reason: { type: String, required: true },
-  claimAmount: { type: Number },
+  claimType: { type: String, required: true },
+  amount: { type: Number },
   document: { type: String },
   status: {
     type: String,

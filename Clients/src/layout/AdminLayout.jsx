5b3acfd732eef32a34859a1532d056830bdabbda
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-29 20:56:51 +0300

    refactor the pages to layout

---
+++ Clients/src/layout/AdminLayout.jsx
@@ -0,0 +1,23 @@
+// dashboard/DashboardLayout.js
+import { Outlet } from "react-router-dom";
+import Sidebar from "@/components/sidebar/Sidebar";
+import Topbar from "@/components/sidebar/Topbar";
+import { motion } from "framer-motion";
+
+export default function AdminLayout() {
+  return (
+    <div className="flex h-screen bg-muted">
+      <Sidebar />
+      <div className="flex flex-1 flex-col ">
+        <Topbar />
+        <motion.main
+          initial={{ opacity: 0, y: 12 }}
+          animate={{ opacity: 1, y: 0 }}
+          className="p-6 overflow-y-auto"
+        >
+          <Outlet />
+        </motion.main>
+      </div>
+    </div>
+  );
+}

bc143ab981b0a6ee6ca94a3bb9e0a1f9b1dc1687
Firagos Jemal <jemalfiragos@gmail.com>
2025-12-31 00:52:17 +0300

    remove transition

--- Clients/src/components/navigation/Navbar.jsx
+++ Clients/src/components/navigation/Navbar.jsx
@@ -193,27 +193,6 @@ const UserDropdown = ({ user, isOpen, onClose, onLogout }) => {
 };
 
 // Page transition wrapper
-export const PageTransition = ({ children }) => {
-  const location = useLocation();
-
-  return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={location.pathname}
-        initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -10 }}
-        transition={{
-          duration: 0.2,
-          ease: "easeInOut",
-        }}
-        className="min-h-screen"
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
-  );
-};
 
 export default function Navbar() {
   const navigate = useNavigate();

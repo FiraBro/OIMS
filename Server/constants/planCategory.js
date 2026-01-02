e386255fa399c1cb90ae088520992c1f6e109c95
Firagos Jemal <jemalfiragos@gmail.com>
2026-01-02 22:51:08 +0300

    create it

---
+++ Server/constants/planCategory.js
@@ -0,0 +1,11 @@
+export const PLAN_CATEGORIES = {
+  HEALTH: "Health Insurance",
+  LIFE: "Life Insurance",
+  AUTO: "Auto Insurance",
+  HOME: "Home Insurance",
+  TRAVEL: "Travel Insurance",
+  BUSINESS: "Business Insurance",
+  DENTAL: "Dental Insurance",
+  VISION: "Vision Insurance",
+  PET: "Pet Insurance",
+};

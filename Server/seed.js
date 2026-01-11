9d12194e294aaf24c731f0a073e977bf5f0f359b
Firagos Jemal <jemalfiragos@gmail.com>
2026-01-11 21:02:00 +0300

    remove un nessesory thing

--- Server/seed.js
+++ Server/seed.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import path from "path";
+// import path from "path";
 import { fileURLToPath } from "url";
 import mongoose from "mongoose";
 import { GoogleGenerativeAI } from "@google/generative-ai";
@@ -7,7 +7,7 @@ import FAQ from "./models/faq.js";
 dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __dirname = path.dirname(__filename);
 // dotenv.config({ path: path.join(__dirname, ".env") });
 
 const sampleFAQs = [
